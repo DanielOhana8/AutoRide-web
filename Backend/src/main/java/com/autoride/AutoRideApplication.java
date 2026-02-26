8aeffc198e5e0de179a15343c32e129e9552d3c6
DanielOhana8 <ohanadaniel9@gmail.com>
2026-02-27 01:49:19 +0200

    Initial commit: Add Spring Boot backend structure

---
+++ Backend/src/main/java/com/autoride/AutoRideApplication.java
@@ -0,0 +1,13 @@
+package com.autoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRideApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/autoride/AutoRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
