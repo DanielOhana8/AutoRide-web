904cf03138719979d17e3907f7b685a2a0f662ee
DanielOhana8 <ohanadaniel9@gmail.com>
2026-03-08 19:44:42 +0200

    Add Docker, Swagger and Seed Data and fix .gitingnore

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO cars (model, latitude, longitude, is_available, price_per_min, version)
+VALUES ('Toyota Corolla', 32.0853, 34.7818, true, 1.50, 0);
+
+INSERT INTO cars (model, latitude, longitude, is_available, price_per_min, version)
+VALUES ('Kia Picanto', 32.0722, 34.7789, true, 1.30, 0);
+
+INSERT INTO cars (model, latitude, longitude, is_available, price_per_min, version)
+VALUES ('Hyundai i10', 32.0911, 34.7733, true, 1.20, 0);
